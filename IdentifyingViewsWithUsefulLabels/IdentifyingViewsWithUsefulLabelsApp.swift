9645369df98c4cacbd85e99bba773c5ecbd1ed4a
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-14 09:01:31 +0200

    Initial Commit

---
+++ IdentifyingViewsWithUsefulLabels/IdentifyingViewsWithUsefulLabelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdentifyingViewsWithUsefulLabelsApp.swift
+//  IdentifyingViewsWithUsefulLabels
+//
+//  Created by test on 14.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct IdentifyingViewsWithUsefulLabelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
